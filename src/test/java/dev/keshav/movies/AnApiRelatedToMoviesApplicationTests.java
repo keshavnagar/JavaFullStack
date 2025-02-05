323f5f545febf91fc7aa7e5c25de3a521c24978e
keshavnagar <keshavnagar887@gmail.com>
2025-02-05 22:32:09 +0530

    now I write hello root

---
+++ src/test/java/dev/keshav/movies/AnApiRelatedToMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.keshav.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnApiRelatedToMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
